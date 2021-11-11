72dff7b22d55464e4f616c4329f2d62cbb3ef151
Eliska Talianova <xtalia00@vut.cz>
2021-11-11 13:30:24 +0100

    dodělano hledání indexu a indikace vzestup/sestup

--- breakpointy.R
+++ breakpointy.R
@@ -1,5 +1,40 @@
 indexstart = function(vek){
-  for (i in 1:length(vek)) {
-    
+  k=0
+  for (i in 2:length(vek)) {
+    index = i
+    k= k+i-1
+    if (sum(vek[1:i])==k){
+      next}
+    else{
+      break
+    }
   }
+  return(index)
+}
+
+
+vzes_ses = function(vek){
+  inde_vek = numeric(length(vek))
+  inde_vek[1]= 1
+  inde_vek[length(vek)]=1
+  
+  for (i in 2:length(vek)){
+    if (vek[i] == ( vek[i-1] +1)){
+      inde_vek[i]=1
+      inde_vek[i-1]=1
+    }
+    else{next}
+      
+  }
+  return(inde_vek)
+}
+
+
+
+breakpointsort = function(vek){
+  pomoc_vek = numeric(length(vek)+2)
+  pomoc_vek[length(vek)+2]=max(vek)+1
+  
+  
+  
 }
